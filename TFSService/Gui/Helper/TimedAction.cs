89b14941a8d3033b07184ce67a30642fce1dfe3c
Аркадий <sheglovaoe@gmail.com>
2019-05-06 18:44:47 +0300

    Доработал класс отложенного выполнения

--- TFSService/Gui/Helper/TimedAction.cs
+++ TFSService/Gui/Helper/TimedAction.cs
@@ -9,17 +9,47 @@ using Timer = System.Timers.Timer;
 
 namespace Gui.Helper
 {
+    /// <summary>
+    /// Класс для выполнения действий с интервалом.
+    /// Предназначен для Web запросов
+    /// </summary>
+    /// <typeparam name="TIn">Что передаем</typeparam>
+    /// <typeparam name="TOut">Что получаем</typeparam>
     public class TimedAction<TIn, TOut>
     {
+        /// <summary>
+        /// Функция выполнения
+        /// </summary>
         private readonly Func<TIn, TOut> _action;
+
+        /// <summary>
+        /// Таймер
+        /// </summary>
         private readonly Timer _timer;
+
+        /// <summary>
+        /// Контекст для GUI потока
+        /// </summary>
         private readonly SynchronizationContext _sync;
 
+        /// <summary>
+        /// Параметр
+        /// </summary>
         private TIn _param;
 
+        /// <summary>
+        /// Запрос на выполнение от пользователя
+        /// </summary>
+        private bool _sheduledByUser;
+
+        /// <summary>
+        /// Событие по окончанию асинхронного действия
+        /// </summary>
         public event EventHandler<TOut> Performed;
 
-        public TimedAction(Func<TIn, TOut> action, int interval = 200)
+        /// <param name="action">Длительное действие</param>
+        /// <param name="interval">Интервал для выполнения действия</param>
+        public TimedAction(Func<TIn, TOut> action, int interval = 400)
         {
             _action = action;
             _timer = new Timer(interval);
@@ -28,17 +58,33 @@ namespace Gui.Helper
             _sync = SynchronizationContext.Current;
         }
 
+        /// <summary>
+        /// Асинхронно выполняем действие и запускаем событие в GUI потоке
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private async void OnPerform(object sender, ElapsedEventArgs e)
         {
+            if (!_sheduledByUser)
+                return;
+
             var result = await Task.Run(() => _action(_param));
             _sync.Post(state => Performed?.Invoke(this, result), null);
+            _sheduledByUser = false;
         }
 
+        /// <summary>
+        /// Ставим событие в очередь, чтобы избежать ложных срабатываний
+        /// </summary>
+        /// <param name="param"></param>
         public void Shedule(TIn param)
         {
             _param = param;
+
             _timer.Stop();
             _timer.Start();
+
+            _sheduledByUser = true;
         }
     }
 }
