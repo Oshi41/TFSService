188178255345e6601aa947c923803ed8e31428d4
Аркадий <sheglovaoe@gmail.com>
2019-07-15 12:17:24 +0300

    Поправил вид месячного списания Поправил поиск по контену рабочих элементов

--- TFSService/TfsAPI/TFS/TfsObservable.cs
+++ TFSService/TfsAPI/TFS/TfsObservable.cs
@@ -96,13 +96,13 @@ namespace TfsAPI.TFS
             _myItems = _myItems.Except(removed, _comparer).ToList();
 
             // Обновляю нетронутые элементы по данным из TFS
-            UpdateItems();
+            // UpdateItems();
 
             // Добавил элементы
             _myItems.AddRange(added);
 
             // Обновляю данные по билдам
-            UpdateBuilds();
+            // UpdateBuilds();
 
             // Т.к. правила проверять постоянно не нужно
             if (timerEllapsed)
